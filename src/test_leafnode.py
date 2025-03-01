49211a9b937b959a191f7a66d74a11f4015f5edc
alfredo-oss <alfredo.derodt@gmail.com>
2025-03-01 14:27:04 -0300

    added raising unit testing

--- src/test_leafnode.py
+++ src/test_leafnode.py
@@ -9,6 +9,13 @@ class TestLeafNode(unittest.TestCase):
         node = LeafNode("a", "juju", **props).to_html()
         res = "<a href=\"https://www.google.com\" >juju</a>"
         self.assertEqual(node, res)
+    
+    def test_no_value(self):
+        props = {
+                "href": "https://www.google.com"
+            }
+        with self.assertRaises(ValueError):
+            LeafNode(tag="a", value=None, **props).to_html()
 
 if __name__ == "__main__":
     unittest.main()
