b91f04f586a7371fafaa321af9c89c116c4ea6f3
alfredo-oss <alfredo.derodt@gmail.com>
2025-03-15 18:29:03 -0300

    addition of tests

---
+++ src/service/tests/test_extract_imgs.py
@@ -0,0 +1,22 @@
+from service.nodes.textnode import TextNode, TextType
+from service.utils.functions import split_nodes_image
+import unittest
+
+class TestImageRegexExtraction(unittest.TestCase):
+    def test_split_images(self):
+        node = TextNode(
+            "This is text with an ![image](https://i.imgur.com/zjjcJKZ.png) and another ![second image](https://i.imgur.com/3elNhQu.png)",
+            TextType.NORMAL,
+        )
+        new_nodes = split_nodes_image([node])
+        self.assertListEqual(
+            [
+                TextNode("This is text with an ", TextType.NORMAL),
+                TextNode("image", TextType.IMAGE, "https://i.imgur.com/zjjcJKZ.png"),
+                TextNode(" and another ", TextType.NORMAL),
+                TextNode(
+                    "second image", TextType.IMAGE, "https://i.imgur.com/3elNhQu.png"
+                ),
+            ],
+            new_nodes,
+        )
--- src/service/utils/functions.py
+++ src/service/utils/functions.py
@@ -120,12 +120,56 @@ def split_nodes_image(old_nodes):
     result = []
     if len(old_nodes) > 1:
         for node in old_nodes:
+            result.append(main_job(node.text))
+        return result
+    elif len(old_nodes) == 1:
+        return main_job(old_nodes[0].text)
+    else:
+        return result
+
+def split_nodes_link(old_nodes):
+    def main_job(text):
+        final_result = []
+        matches = extract_markdown_links(text)
+        res = [text]
+
+        delimiters = []
+        for link_alt, link_url in matches:
+            delimiters.append(f"[{link_alt}]({link_url})")
+
+        for delimiter in delimiters:
+            tmp = []
+            for text_element in res:
+                tmp.extend(text_element.split(delimiter))
+            res=tmp
+
+
+        cleaned = []
+        for element in res:
+            if element == "":
+                continue
+            else:
+                cleaned.append(element)
+
+        match_queue = deque()
+        for imgs in matches:
+            match_queue.append(imgs)
+
+        for cleansed in cleaned:
+            final_result.append(TextNode(cleansed, TextType.NORMAL))
+            print(f"TextNode({cleansed}, TextType.NORMAL)")
+            if match_queue:
+                alt, url = match_queue.popleft()
+                final_result.append(TextNode(alt, TextType.LINK, url))
+                print(f"TextNode({alt}, TextType.IMAGE, {url})")
+        return final_result
+    
+    result = []
+    if len(old_nodes) > 1:
+        for node in old_nodes:
             result.append(main_job(node))
         return result
     elif len(old_nodes) == 1:
         return main_job(old_nodes[0])
     else:
         return result
-
-def split_nodes_link(old_nodes):
-    pass
