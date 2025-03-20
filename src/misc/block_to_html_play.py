9e7ad2ec6da30b79530256a3bd98857323f3ec35
alfredo-oss <alfredo.derodt@gmail.com>
2025-03-20 20:20:59 -0300

    adding md to html node transformation

--- src/misc/block_to_html_play.py
+++ src/misc/block_to_html_play.py
@@ -2,6 +2,8 @@ from service.utils.functions import (
     markdown_to_blocks,
     block_to_block
     )
+from service.blocks.block_types import BlockType
+from service.nodes.parentnode import ParentNode
 
 ## once this is separated, we have to keep in mind that it needs to be joined again
 md = """
@@ -19,4 +21,16 @@ md_blocks = markdown_to_blocks(md)
 
 for block in md_blocks:
     print(f"BLOCK {block} is of type: {block_to_block(block)}")
-
+    match block_to_block(block):
+        case BlockType.paragraph:
+            print(ParentNode('p', block).to_html())
+        case BlockType.heading:
+            print(ParentNode('h1', block).to_html())
+        case BlockType.code:
+            print(ParentNode('code', block).to_html())
+        case BlockType.quote:
+            print(ParentNode('blockquote', block).to_html())
+        case BlockType.unordered_list:
+            print(ParentNode('ul', block).to_html())
+        case BlockType.ordered_list:
+            print(ParentNode('ol', block).to_html())
